ce133d453c3dd2b1a6d4ae9e79fa2cd575e4cddd
Hyeontae <dchs504@gmail.com>
2021-04-08 12:13:30 +0900

    rm position imformation

--- Seq2seq/models/encoderRNN.py
+++ Seq2seq/models/encoderRNN.py
@@ -9,7 +9,7 @@ from models.baseRNN import BaseRNN
 class EncoderRNN(BaseRNN):
 
     def __init__(self, vocab_size, max_len, hidden_size,
-                 embedding_size, input_dropout_p=0, dropout_p=0, pos_embedding_size=None, pos_embedding=None,
+                 embedding_size, input_dropout_p=0, dropout_p=0,
                  n_layers=1, bidirectional=False, rnn_cell='lstm', variable_lengths=False,
                  embedding=None, update_embedding=True):
         super(EncoderRNN, self).__init__(vocab_size, max_len, hidden_size,
@@ -32,10 +32,7 @@ class EncoderRNN(BaseRNN):
         batch_size = input_var.size(0)
         seq_len = input_var.size(1)
 
-        if self.pos_embedding == None:
-            embedded = self.embedding(input_var)
-        else:
-            embedded = torch.cat((self.embedding(input_var), self.pos_embedding(input_partition)), dim=2)
+        embedded = self.embedding(input_var)
 
         if self.variable_lengths:
             embedded = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths, batch_first=True)
